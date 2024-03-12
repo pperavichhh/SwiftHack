1e8d127bcc30830394ab01944cf456d40e71453c
U Peravich <pperavichhh@gmail.com>
2024-03-11 22:16:38 -0700

    Initial Commit

---
+++ project/projectApp.swift
@@ -0,0 +1,17 @@
+//
+//  projectApp.swift
+//  project
+//
+//  Created by admin on 3/11/24.
+//
+
+import SwiftUI
+
+@main
+struct projectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
