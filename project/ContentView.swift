4a477b9fbb5706a9d45739476b7a133b0cba36f2
U Peravich <85332850+pperavichhh@users.noreply.github.com>
2024-03-12 22:26:06 +0700

    Update ContentView.swift
    Add All Basic UI

--- project/ContentView.swift
+++ project/ContentView.swift
@@ -1,13 +1,22 @@
+//
+//  ContentView.swift
+//  Manu
+//
+//  Created by User on 11/3/2567 BE.
+//
+
 import SwiftUI
+// tabbed bar
 
 struct ContentView: View {
     var body: some View {
         TabView {
             
-            WorkoutPage()                .tabItem {
-                    Image(systemName: "figure.run")
-                    Text("Workout")
-                }
+            WorkoutPage()                
+                .tabItem {
+                Image(systemName: "figure.run")
+                Text("Workout")
+            }
             
             AddDataView()
                 .tabItem {
@@ -19,6 +28,7 @@ struct ContentView: View {
                     Image(systemName: "house.fill")
                     Text("Home")
                 }
+        
             
             VoucherListView()
                 .tabItem {
@@ -26,16 +36,16 @@ struct ContentView: View {
                     Text("Voucher")
                 }
             
-            Text("Profile")
+            SettingsView()
                 .tabItem {
-                    Image(systemName: "person.crop.circle")
-                    Text("Profile")
+                    Image(systemName: "gear")
+                    Text("Setting")
                 }
         }.accentColor(.black)
     }
 }
 
-
+//Home Page
 struct HomePageView: View {
     let username = "John"
     var days = 3
@@ -97,6 +107,8 @@ struct HomePageView: View {
     }
 }
 
+
+// Add Data
 struct AddDataView: View {
     @State private var name: String = ""
     @State private var age: String = ""
@@ -142,6 +154,7 @@ struct AddDataView: View {
 }
 
 
+// Voucher
 struct Voucher: Identifiable {
     let id = UUID()
     let code: String
@@ -236,11 +249,13 @@ struct VoucherDetailView: View {
     }
 }
 
+
+// Workout Page
 struct Exercise: Identifiable {
     var id = UUID()
     var name: String
     var description: String
-    var estimatedTime: String
+    var estimatedTime: Int
     var estimatedKcal: Int
     var sets: Int?
     var reps: Int?
@@ -261,24 +276,82 @@ struct WorkoutDay: Identifiable {
 
 struct WorkoutPage: View {
     let workoutDays: [WorkoutDay] = [
+        
+        // day 1
+        
         WorkoutDay(day: "Day 1", workouts: [
-            Workout(name: "Morning Routine", exercises: [
-                Exercise(name: "Push-ups", description: "Do 3 sets of 10 push-ups", estimatedTime: "15 minutes", estimatedKcal: 50),
-                Exercise(name: "Squats", description: "Do 3 sets of 15 squats", estimatedTime: "15 minutes", estimatedKcal: 60),
+            Workout(
+                name: "Morning Routine",
+                exercises: [
+                    Exercise(
+                    name: "Push-ups",
+                    description: "Do 3 sets of 10 push-ups",
+                    estimatedTime: 15 ,
+                    estimatedKcal: 50),
+                    
+                    Exercise(
+                    name: "Squats",
+                    description: "Do 3 sets of 15 squats",
+                    estimatedTime: 15,
+                    estimatedKcal: 60),
             ]),
-            Workout(name: "Evening Workout", exercises: [
-                Exercise(name: "Plank", description: "Hold plank position for 1 minute", estimatedTime: "5 minutes", estimatedKcal: 20),
-                Exercise(name: "Lunges", description: "Do 3 sets of 12 lunges", estimatedTime: "15 minutes", estimatedKcal: 50),
+            
+            Workout(
+                name: "Evening Workout",
+                exercises: [
+                    Exercise(
+                    name: "Plank",
+                    description: "Hold plank position for 1 minute",
+                    estimatedTime: 5,
+                    estimatedKcal: 20),
+                
+                    Exercise(
+                    name: "Lunges",
+                    description: "Do 3 sets of 12 lunges",
+                    estimatedTime: 15,
+                    estimatedKcal: 50),
             ]),
         ]),
+        
+        // day 2
+        
         WorkoutDay(day: "Day 2", workouts: [
-            Workout(name: "Cardio Session", exercises: [
-                Exercise(name: "Running", description: "Run for 20 minutes", estimatedTime: "20 minutes", estimatedKcal: 150),
-                Exercise(name: "Jumping Jacks", description: "Do 3 sets of 30 jumping jacks", estimatedTime: "10 minutes", estimatedKcal: 80),
+            Workout(
+            name: "Cardio Session",
+            exercises: [
+                Exercise(
+                name: "Running",
+                description: "Run for 20 minutes",
+                estimatedTime: 20 ,
+                estimatedKcal: 150),
+                
+                Exercise(
+                name: "Jumping Jacks",
+                description: "Do 3 sets of 30 jumping jacks",
+                estimatedTime: 10 ,
+                estimatedKcal: 80),
             ]),
-            Workout(name: "Strength Training", exercises: [
-                Exercise(name: "Deadlifts", description: "Do 4 sets of 8 deadlifts", estimatedTime: "25 minutes", estimatedKcal: 120, sets: 4, reps: 8, weight: "50 lbs"),
-                Exercise(name: "Bicep Curls", description: "Do 3 sets of 12 bicep curls", estimatedTime: "15 minutes", estimatedKcal: 70, sets: 3, reps: 12, weight: "20 lbs"),
+            
+            Workout(
+                name: "Strength Training",
+                exercises: [
+                    Exercise(
+                        name: "Deadlifts",
+                        description: "Do 4 sets of 8 deadlifts",
+                        estimatedTime: 25 ,
+                        estimatedKcal: 120,
+                        sets: 4,
+                        reps: 8,
+                        weight: "50 lbs"),
+                
+                    Exercise(
+                        name: "Bicep Curls",
+                        description: "Do 3 sets of 12 bicep curls",
+                        estimatedTime: 15 ,
+                        estimatedKcal: 70,
+                        sets: 3,
+                        reps: 12,
+                        weight: "20 lbs"),
             ]),
         ]),
         // Add more workout days as needed
@@ -309,7 +382,9 @@ struct WorkoutDetail: View {
                                 .font(.headline)
                             Text(exercise.description)
                                 .font(.subheadline)
-                            if let sets = exercise.sets, let reps = exercise.reps, let weight = exercise.weight {
+                            if let sets = exercise.sets, 
+                                let reps = exercise.reps,
+                                let weight = exercise.weight {
                                 Text("Sets: \(sets), Reps: \(reps), Weight: \(weight)")
                                     .font(.caption)
                                     .foregroundColor(.gray)
@@ -328,13 +403,141 @@ struct WorkoutDetail: View {
     }
 
     func totalTimeForDay(_ workouts: [Workout]) -> Int {
-        return workouts.flatMap { $0.exercises }.reduce(0) { $0 + (Int($1.estimatedTime) ?? 0) }
+        return workouts.flatMap { $0.exercises }.reduce(0) { $0 + $1.estimatedTime }
     }
 
     func totalKcalForDay(_ workouts: [Workout]) -> Int {
         return workouts.flatMap { $0.exercises }.reduce(0) { $0 + $1.estimatedKcal }
     }
 }
+
+// Settings Page
+struct SettingsView: View {
+    @State private var isShowingLogoutAlert = false
+
+    var body: some View {
+        NavigationView {
+            List {
+                Section(header: Text("General")) {
+                    NavigationLink(destination:GeneralSettingsView()) {
+                        HStack {
+                            Image(systemName: "gear")
+                            Text("General Settings")
+                        }
+                    }
+                }
+
+                Section(header: Text("Account")) {
+                    NavigationLink(destination: Text("Account Settings")) {
+                        HStack {
+                            Image(systemName: "person")
+                            Text("Account Settings")
+                        }
+                    }
+                }
+
+                Section(header: Text("Feedback")) {
+                    NavigationLink(destination: Text("Send Feedback")) {
+                        HStack {
+                            Image(systemName: "message")
+                            Text("Send Feedback")
+                        }
+                    }
+                    NavigationLink(destination: Text("Rate App")) {
+                        HStack {
+                            Image(systemName: "star")
+                            Text("Rate App")
+                        }
+                    }
+                }
+
+                Section {
+                    Button(action: {
+                        isShowingLogoutAlert = true
+                    }) {
+                        HStack {
+                            Image(systemName: "arrow.down.left.circle")
+                            Text("Logout")
+                                .foregroundColor(.red)
+                        }
+                    }
+                    .alert(isPresented: $isShowingLogoutAlert) {
+                        Alert(title: Text("Logout"), message: Text("Are you sure you want to logout?"), primaryButton: .cancel(), secondaryButton: .destructive(Text("Logout"), action: {
+                            // Handle logout action here
+                            // You may want to implement your logout logic
+                        }))
+                    }
+                }
+            }
+            .listStyle(GroupedListStyle())
+            .navigationBarTitle("Settings")
+        }
+    }
+}
+
+enum Gender: String, CaseIterable {
+    case male = "Male"
+    case female = "Female"
+}
+
+struct GeneralSettingsView: View {
+    @State private var selectedDate = Date()
+    @State private var selectedGender: Gender = .male
+    
+    var currentYear: Int {
+            Calendar.current.component(.year, from: Date())
+        }
+
+        var age: Int {
+            currentYear - Calendar.current.component(.year, from: selectedDate)
+        }
+
+
+    var body: some View {
+        Form {
+            Section(header: Text("Personal Information")) {
+                HStack {
+                    Image(systemName: "person")
+                    VStack(alignment: .leading) {
+                        Text("Gender")
+                        Picker("Gender", selection: $selectedGender) {
+                            ForEach(Gender.allCases, id: \.self) { gender in
+                                Text(gender.rawValue)
+                            }
+                        }
+                        .pickerStyle(SegmentedPickerStyle())
+                    }
+                }
+
+                HStack {
+                    Image(systemName: "calendar")
+                    DatePicker("Birthday", selection: $selectedDate, displayedComponents: [.date])
+                        .labelsHidden()
+                }
+                HStack {
+                    Image(systemName: "clock")
+                    Text("Age: \(age)")
+                        .foregroundColor(.secondary)
+                        .font(.footnote)
+                        .padding(.leading, 5)
+                }
+                
+                
+            }
+
+            Section(header: Text("App Information")) {
+                HStack {
+                    Image(systemName: "info.circle")
+                    Text("App Version")
+                    Spacer()
+                    Text("Pre-alpha") // Replace with your actual app version
+                }
+            }
+        }
+        .navigationBarTitle("General Settings")
+    }
+}
+
 
 //Support Function
 
